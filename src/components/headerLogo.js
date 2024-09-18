c6bd82cd2021e608b80e54bc745caed5ba57ba22
LD-Sfalzon <sfalzon@launchdarkly.com>
2024-09-18 15:15:13 +1000

    i like this better

--- src/components/headerLogo.js
+++ src/components/headerLogo.js
@@ -7,12 +7,13 @@ const headerLogo = ({ flags }) => {
   // The React SDK automatically converts flag keys with dashes and periods to camelCase.
   // See this page for details: https://docs.launchdarkly.com/sdk/client-side/react/react-web#flag-keys
   return flags.releaseHeaderLogo ? (
-  <div>
-    <img src={ldlogo} className="LD-logo" alt="logo" />
+  <div text-align="center">
+    <img src={ldlogo} className="LD-logo" alt="logo"  width ="60%"
+ />
   </div>
   ) : (
-  <div>
-    <img src={oldldlogo} className="LD-logo" alt="logo" />
+  <div text-align="center">
+    <img src={oldldlogo} className="LD-logo" alt="logo" width ="60%"/>
   </div>
   );
 };
