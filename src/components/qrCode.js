3bc19ec1dee15e1d49cb3cb1117301f3054dcdb6
LD-Sfalzon <sfalzon@launchdarkly.com>
2024-09-20 12:48:12 +1000

    Update qrCode.js

--- src/components/qrCode.js
+++ src/components/qrCode.js
@@ -2,7 +2,7 @@ import QRCode from "react-qr-code";
 import { withLDConsumer } from "launchdarkly-react-client-sdk";
 
 //Change QRURL to the URL where you'll be hosting this app
-const QRURL = "https://www.youtube.com/watch?v=j5a0jTc9S10";
+const QRURL = "https://ld-sfalzon.github.io/react_qr_app/";
 
 const qrCodeHome = ({ flags }) => {
 
