54d4c0ae15cbfa497e0c7d4566a88aa664e7711a
LD-Sfalzon <sfalzon@launchdarkly.com>
2025-04-30 13:01:31 +1000

    Update getUserId.js

--- src/util/getUserId.js
+++ src/util/getUserId.js
@@ -1,13 +1,13 @@
-import {v4 as uuidv4} from 'uuid';
-import ls from 'local-storage';
+import { v4 as uuidv4 } from "uuid";
+import ls from "local-storage";
 
 function getUserId() {
   let id;
-  if (ls.get('LD_User_Key')) {
-    id = ls.get('LD_User_Key');
+  if (ls.get("LD_User_Key")) {
+    id = ls.get("LD_User_Key");
   } else {
     id = uuidv4();
-    ls.set('LD_User_Key', id)
+    ls.set("LD_User_Key", id);
   }
   return id;
 }
